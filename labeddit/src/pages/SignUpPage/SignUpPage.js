30ee279a859b4b4f94c1dc1514648acc5e529858
Aurila Sabrina Alves <aurila.sabrina@gmail.com>
2022-07-28 18:12:42 -0300

    SignUpPage e Header

--- labeddit/src/pages/SignUpPage/SignUpPage.js
+++ labeddit/src/pages/SignUpPage/SignUpPage.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Button } from '../../components/Button/Button';
+import { Header } from '../../components/Header/Header';
+import { SignUpStyle, BoasVindas, FormStyle, InputStyle, MensagemTermos, MensagemPubli, InputPublicidade, AStyle } from './styled';
+
 
 export const SignUpPage = () => {
     return (
-    <div>
-        <p>Cadastre-se!</p>
-    </div>
+    <SignUpStyle>
+        <Header></Header>
+        <BoasVindas>
+        <p>Olá, boas vindas ao LabEddit ;) </p>
+        </BoasVindas>
+    
+         <FormStyle>
+            <InputStyle
+            placeholder="Nome de usuário"
+            id="userName"
+            type="string"
+            name="name"
+            />
+             <InputStyle
+            placeholder="E-mail"
+            id="userName"
+            type="email"
+            name="name"
+            />
+             <InputStyle
+            placeholder="Senha"
+            id="userName"
+            type="string"
+            name="name"
+            />
+         
+        </FormStyle>
+        <div>
+            <MensagemTermos>Ao continuar, você concorda com o nosso <AStyle href='#contrato'>Contrato de usuário</AStyle> e nossa <AStyle href='#privacidade'>Política de Privacidade</AStyle>.</MensagemTermos>
+            <MensagemPubli>
+            <label><InputPublicidade
+            type='checkbox'/>
+            Eu concordo em receber emails sobre coisas legais no Labeddit
+            </label>
+            </MensagemPubli>
+        </div>   
+            <Button text= 'Cadastrar'/>
+    </SignUpStyle>
     )
 }
